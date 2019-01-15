73ffa4d1197b5405f5513731319fc9129f633947
Luke Pighetti <lukepighetti@gmail.com>
2019-01-14 20:46:27 -0500

    Update middleware_test.dart
    duplicate test

--- test/middleware_test.dart
+++ test/middleware_test.dart
@@ -75,28 +75,5 @@ void main() {
 
       expect(middleware1.counter, equals(2));
     });
-
-    test('actions can be dispatched through entire chain', () {
-      final middleware1 = new ExtraActionIfDispatchedIncrementMiddleware();
-      final middleware2 = new IncrementMiddleware();
-      final store = new Store<String>(
-        stringReducer,
-        initialState: 'hello',
-        middleware: [middleware1, middleware2],
-      );
-
-      final order = <String>[];
-      middleware1.invocations.listen((action) => order.add('first'));
-      middleware2.invocations.listen((action) => order.add('second'));
-
-      store.dispatch('test');
-
-      expect(order[0], equals('first'));
-      expect(order[1], equals('second'));
-      expect(order[2], equals('first'));
-      expect(order[3], equals('second'));
-
-      expect(middleware1.counter, equals(2));
-    });
   });
 }
